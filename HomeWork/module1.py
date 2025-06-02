4a6f8693aeee11424db220cd67182a93e31d4ecf
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-02 19:06:54 +0530

    Homework 2 - (2 June)

---
+++ HomeWork/module1.py
@@ -0,0 +1,4 @@
+
+moduleOne_int = 10
+moduleOne_float = 3.14
+moduleOne_bool = True
---
+++ HomeWork/module2.py
@@ -0,0 +1,5 @@
+import module1
+
+print("Interger Value in Module1 is", module1.moduleOne_int)
+print("Float Value in Module1 is", module1.moduleOne_float)
+print("Boolean Value in Module1 is",module1.moduleOne_bool)
