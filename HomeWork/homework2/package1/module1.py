9c73587e209e1914b3209c6c58b305fc9c7f6c08
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-02 19:51:57 +0530

    Q2 Homework 2 - (2 June)

---
+++ HomeWork/homework2/package1/module1.py
@@ -0,0 +1,4 @@
+#This is Module 1 inside Package 1
+moduleOne_int = 10
+moduleOne_float = 3.14
+moduleOne_bool = True
---
+++ HomeWork/homework2/package2/module2.py
@@ -0,0 +1,25 @@
+from package1 import module1
+
+moduleTwo_int = 20
+moduleTwo_float = 345.234
+moduleTwo_bool = False
+
+print("Interger Value in Module1 is", module1.moduleOne_int)
+print("Float Value in Module1 is", module1.moduleOne_float)
+print("Boolean Value in Module1 is", module1.moduleOne_bool)
+print("Data from Module 2")
+print("Interger Value in Module2 is", moduleTwo_int)
+print("Float Value in Module2 is", moduleTwo_float)
+print("Boolean Value in Module2 is", moduleTwo_bool)
+
+
+##############################################################
+# OUTPUT
+
+# Interger Value in Module1 is 10
+# Float Value in Module1 is 3.14
+# Boolean Value in Module1 is True
+# Data from Module 2
+# Interger Value in Module2 is 20
+# Float Value in Module2 is 345.234
+# Boolean Value in Module2 is False
