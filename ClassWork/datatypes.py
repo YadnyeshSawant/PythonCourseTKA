86c3d118aee1e362df0b470b68f2dafa61f880fd
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-02 18:46:35 +0530

    1 June

---
+++ ClassWork/datatypes.py
@@ -0,0 +1,24 @@
+a =10
+print(type(a),a)
+
+b = 0b10101
+print(type(b),b)
+
+c = 0o1234
+print(type(c),c)
+
+d = 0x3245a34
+print(type(d),d)
+
+e = True
+print(type(e),e)
+print(e + e)
+
+f = -20.5
+print(type(f),f)
+
+
+#Homework
+# atul@thekiranacademy.com before 2:30pm
+# 1.create int, float and bool variables in module 1 and print them into module2
+# 2.create int, float and bool variables in package1 1 - > module 1 and print them into package2 -> module2
