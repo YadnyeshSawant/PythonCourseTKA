d73d92a4f1135f97d575821566ab2de2ace15986
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-02 19:32:54 +0530

    Homework 2 - (2 June)

--- HomeWork/module2.py
+++ HomeWork/module2.py
@@ -7,7 +7,7 @@ moduleTwo_bool = False
 print("Interger Value in Module1 is", module1.moduleOne_int)
 print("Float Value in Module1 is", module1.moduleOne_float)
 print("Boolean Value in Module1 is", module1.moduleOne_bool)
-
+print("Data from Module 2")
 print("Interger Value in Module2 is", moduleTwo_int)
 print("Float Value in Module2 is", moduleTwo_float)
 print("Boolean Value in Module2 is", moduleTwo_bool)
@@ -19,7 +19,7 @@ print("Boolean Value in Module2 is", moduleTwo_bool)
 # Interger Value in Module1 is 10
 # Float Value in Module1 is 3.14
 # Boolean Value in Module1 is True
-
+# Data from Module 2
 # Interger Value in Module2 is 20
 # Float Value in Module2 is 345.234
 # Boolean Value in Module2 is False
