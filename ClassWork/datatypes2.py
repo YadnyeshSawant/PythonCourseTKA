27bfffb8591b1dbb62a1211d7d2803a5162cb00c
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-04 09:57:54 +0530

    3 June

---
+++ ClassWork/datatypes2.py
@@ -0,0 +1,14 @@
+# 3 JUNE 2025
+import string
+s = "INSTAGRAM"
+print(s[::-1])
+# print(s[1])
+# print(s[2])
+# print(s[-1])
+# print(s[-2])
+# print(len(s))
+
+#Homework use negative indexing in slicing 
+
+#for 4 june 
+# how to iterate string in python
