39f3d10610f16b290211e6505df948e4a26500f2
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-04 10:18:50 +0530

    Homework  3 - (3 June )

---
+++ HomeWork/homework3.py
@@ -0,0 +1,17 @@
+import string
+s = "INSTAGRAM"
+print("Length of string ",len(s))
+print(s[-3: :+1])
+print(s[-1:-4:-1])
+print(s[-7:-3:+1])
+print(s[-4:-8:-1])
+print(s[-1: :-1])
+
+#######################################################
+# OUTPUT
+# Length of string  9
+# RAM
+# MAR
+# STAG
+# GATS
+# MARGATSNI
