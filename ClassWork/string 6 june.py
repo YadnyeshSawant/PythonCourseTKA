c26a23940037a105a133e227bdb4d3d7e706cb56
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-06 15:13:59 +0530

    list june 6

---
+++ ClassWork/string 6 june.py
@@ -0,0 +1,16 @@
+# a =10
+# print(id(a),a)
+
+# a =20
+# print(id(a), a)
+
+# a=30
+# print(id(a), a)
+
+l =[]
+l.append(10)
+l.append("Hello")
+l.append(-20.5)
+l.append(True)
+l.append("Hello")
+print(l)
