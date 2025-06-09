17d8fc77c3ac204127d90733ebb4c62344816cf9
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-09 16:01:17 +0530

    tuple june 9

--- ClassWork/TUPLE 9 june.py
+++ ClassWork/TUPLE 9 june.py
@@ -1,5 +1,20 @@
-t = ("Yadnyesh",2)
-print(type(t),t)
+# TUPLE DATA TYPE:  JUNE 9 
+t = (1,2,3,"Yadnyesh","Sawant")
 
-l = [10]
-print(type(l), l)
+for data in t:
+    print(data)
+    
+for i in range(len(t)):
+    print(i," -- >",t[i])
+print(id(t))
+
+l = list(t)
+print(l)
+l.append("TKA")
+print(l)
+
+t = tuple(l)
+print(t)
+for i in range(len(t)):
+    print(i," -- >",t[i])
+print(id(t))
