f5bad1ed69ceb0f08199f57f94c62172e0f2f7fa
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-10 20:05:56 +0530

    Homework 5 - (10 June)

---
+++ HomeWork/homework5.py
@@ -0,0 +1,25 @@
+import moviesDb
+
+actorName = input("Enter the name of the Actor  ")
+count = 0
+movie = []
+year = []
+for k,v in moviesDb.moviesData.items():
+    # print(k,v,"\n")
+    for k1,v1 in v.items():
+        for actor in v1:
+            if actor == actorName:
+                count += 1
+                movie.append(k1)
+                year.append(k)
+                
+print("----"*10)
+
+print("Actor Name =", actorName)
+for i in range(0,count):
+    print(f"Movie Name = {movie[i]} ({year[i]})")
+# print("Year of Release =", year)
+# print("Name of Movie =",movie)
+print("Number of the time stared in movie = ", count)
+
+print("----"*10)
