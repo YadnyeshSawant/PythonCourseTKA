dc0562d56b9ef532aaef65ce1f20bbd3f9595a44
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-10 20:06:17 +0530

    Homework 5 movies dataset

---
+++ HomeWork/moviesDb.py
@@ -0,0 +1,33 @@
+movies2021 = {
+    "Brahmastra": ["Ranbir Kapoor", "Alia Bhat", "Amitabh Bachchan"],
+    "Dune": ["Timothée Chalamet", "Rebecca Ferguson", "Zendaya"],
+    "Atrangi Re": ["Akshay Kumar","Sara Ali Khan","Dhanush",],
+}
+movies2022 = {
+    "Samshera": ["Ranbir Kapoor", "Sanjay Dutt", "Vaani Kapoor"],
+    "The Batman": ["Robert Pattinson", "Paul Dano", "John Turturro"],
+    "Bachchhan Paandey": ["Akshay Kumar", "Kriti Sanon", "Arshad Warsi"],
+}
+movies2023 = {
+    "Jawan": ["Shah Rukh Khan", "Nayanthara", "Vijay Sethupathi"],
+    "Oppenheimer": ["Cillian Murphy", "Emily Blunt", "Robert Downey Jr."],
+    "Barbie": ["Margot Robbie", "Ryan Gosling", "Simu Liu"],
+}
+movies2024 = {
+    "Fighter": ["Hrithik Roshan", "Deepika Padukone", "Anil Kapoor"],
+    "Dune: Part Two": ["Timothée Chalamet", "Zendaya", "Florence Pugh"],
+    "The Fall Guy": ["Ryan Gosling", "Emily Blunt", "Aaron Taylor-Johnson"],
+}
+movies2025 = {
+    "Deadpool & Wolverine": ["Ryan Reynolds", "Hugh Jackman", "Emma Corrin"],
+    "Singham Again": ["Ajay Devgn", "Ranveer Singh", "Deepika Padukone"],
+    "Mission: Impossible – Dead Reckoning Part Two": ["Tom Cruise","Hayley Atwell","Ving Rhames",],
+}
+
+moviesData = {
+    2021: movies2021,
+    2022: movies2022,
+    2023: movies2023,
+    2024: movies2024,
+    2025: movies2025,
+}
