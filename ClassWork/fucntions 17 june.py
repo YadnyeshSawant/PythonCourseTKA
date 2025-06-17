65e103f53986d98761496ab9ae67de5cf11c43e2
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-17 20:19:34 +0530

    functions june 17

---
+++ ClassWork/fucntions 17 june.py
@@ -0,0 +1,9 @@
+def addTwo(a,b):
+    print("We are inside fun")
+    # global s1
+    s1 = a + b
+    # return s1
+    # print(f"Addition of {a} and {b} is {a+b}")
+s2 = addTwo(2,3)
+print("Addition =",s2)
+# addTwo(int(input("Enter no 1    ")),int(input("Enter no 2    ")))
