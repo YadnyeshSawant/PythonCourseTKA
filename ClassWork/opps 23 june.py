2f70a794e07585a39da538f114879cfc9b021a7b
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-23 20:49:06 +0530

    opps june 23

---
+++ ClassWork/opps 23 june.py
@@ -0,0 +1,10 @@
+# Task 1:
+class Student:
+
+    age = 23
+    location = "Pune"
+
+    def Study():
+        pass
+
+# Hw Task 2: An Employee has name, salary and phone. Phone means mobile number, email address. employee has one operation which displays salary and email and name of employee. 
