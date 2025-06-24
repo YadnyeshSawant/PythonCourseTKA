75f6e309c1d5ddef74dd4a957b2edb5b6a6698f4
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-06-24 19:39:16 +0530

    oops june 24

---
+++ ClassWork/oops 24 june.py
@@ -0,0 +1,12 @@
+class Student:
+
+    def __init__(self,roll,name,marks):
+        self.name = name
+        self.roll = roll
+        self.marks = marks
+
+s1 = Student(1,"Yadnyesh",120)
+print(s1.roll)
+print(s1.name)
+print(s1.marks)
+        
